b64f5e93362332ea14d83cfe61125f9db3a9bd5e
lkeleti <keleti.kaszlo@vipmail.hu>
2023-12-17 18:22:46 +0100

    day17 init

---
+++ day16/advent-of-code-day16/src/main/java/Cord.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+
+public class Cord {
+    private int posX;
+    private int posY;
+
+    public Cord(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cord cord)) return false;
+        return getPosX() == cord.getPosX() && getPosY() == cord.getPosY();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPosX(), getPosY());
+    }
+
+    public Cord add(Cord cord) {
+        return new Cord(this.posX + cord.getPosX(), this.posY + cord.getPosY());
+    }
+}
--- day16/advent-of-code-day16/src/main/java/Service.java
+++ day16/advent-of-code-day16/src/main/java/Service.java
@@ -2,13 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
 
 public class Service {
+    private final List<List<Character>> board = new ArrayList<>();
+    private final Map<Cord,List<Cord>> visited = new HashMap<>();
+
+    private int rowNumber;
+    private int colNumber;
+
+    private List<Cord[]> startPositions = new ArrayList<>();
     public void readInput(Path path) {
         try (BufferedReader br = Files.newBufferedReader(path)) {
             String line;
             while ((line = br.readLine()) != null) {
+                List<Character> row = new ArrayList<>();
+                for (Character c: line.toCharArray()){
+                    row.add(c);
+                }
+                board.add(row);
             }
+            rowNumber = board.size();
+            colNumber = board.getFirst().size();
 
         } catch (IOException ioe) {
             throw new IllegalStateException("Cannot read file: " + path, ioe);
@@ -16,7 +31,89 @@ public class Service {
     }
 
     public int partOne() {
+        Cord defPos = new Cord(0,0);
+        Cord defDirection = new Cord(1,0);
+        startPositions.add(new Cord[] {defPos, defDirection});
+
+        while (!startPositions.isEmpty()) {
+            Cord[] defCords = startPositions.getFirst();
+            defPos = defCords[0];
+            defDirection = defCords[1];
+            while (!isVisited(defPos, defDirection) && !isOverTheBoard(defPos, defDirection)) {
+                defPos = defPos.add(defDirection);
+                updateVisited(defPos, defDirection);
+
+                Character sign = board.get(defPos.getPosY()).get(defPos.getPosX());
+                switch (sign) {
+                    case '-':
+                        defDirection = checkDash(defPos, defDirection);
+                        break;
+                    case '|':
+                        defDirection = checkPipe(defPos, defDirection);
+                        break;
+                    case '\\':
+                        defDirection = checkBackSlash(defPos, defDirection);
+                        break;
+                    case '/':
+                        defDirection = checkSlash(defPos, defDirection);
+                        break;
+                }
+
+            }
+            startPositions.remove(defCords);
+        }
         return 0;
+    }
+
+    private Cord checkSlash(Cord defPos, Cord defDirection) {
+        //ToDo
+        return defDirection;
+    }
+
+    private Cord checkBackSlash(Cord defPos, Cord defDirection) {
+        //ToDo
+        return defDirection;
+    }
+
+    private Cord checkPipe(Cord defPos, Cord defDirection) {
+        if (defDirection.getPosX() == 0) {
+            return defDirection;
+        } else {
+            Cord newDirectionOne = new Cord(0,1);
+            Cord newDirectionTwo = new Cord(0,-1);
+            startPositions.add(new Cord[] {defPos, newDirectionTwo});
+            return newDirectionOne;
+        }
+    }
+
+    private Cord checkDash(Cord defPos, Cord defDirection) {
+        if (defDirection.getPosY() == 0) {
+            return defDirection;
+        } else {
+            Cord newDirectionOne = new Cord(1,0);
+            Cord newDirectionTwo = new Cord(-1,0);
+            startPositions.add(new Cord[] {defPos, newDirectionTwo});
+            return newDirectionOne;
+        }
+    }
+
+    private void updateVisited(Cord defPos, Cord defDirection) {
+        if (!visited.containsKey(defPos)) {
+            visited.put(defPos, new ArrayList<>());
+        }
+
+        if (!isVisited(defPos, defDirection)) {
+            visited.get(defPos).add(defDirection);
+        }
+    }
+
+    private boolean isOverTheBoard(Cord defPos, Cord defDirection) {
+        Cord newPos = defPos.add(defDirection);
+        return (newPos.getPosX() < 0 || newPos.getPosX() > colNumber-1 || newPos.getPosY() < 0 || newPos.getPosY() > rowNumber-1);
+    }
+
+    private boolean isVisited(Cord defPos, Cord defDirection) {
+        return visited.containsKey(defPos.add(defDirection)) && visited.get(defPos).contains(defDirection);
     }
 
     public int partTwo() {
