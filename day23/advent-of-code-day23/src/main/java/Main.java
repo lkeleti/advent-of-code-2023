c5f055da2914b16576a432e4c19bcdb9d0da2ee4
lkeleti <keleti.kaszlo@vipmail.hu>
2024-01-04 08:24:18 +0100

    Day23 init

--- day23/advent-of-code-day23/src/main/java/Main.java
+++ day23/advent-of-code-day23/src/main/java/Main.java
@@ -1,4 +1,4 @@
-
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +8,5 @@ public class Main {
         System.out.println(service.partOne());
         System.out.println("Answer of part 2:");
         System.out.println(service.partTwo());
-
     }
 }
--- day23/advent-of-code-day23/src/main/java/Service.java
+++ day23/advent-of-code-day23/src/main/java/Service.java
@@ -2,16 +2,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Service {
+
+    private final List<List<Character>> board = new ArrayList<>();
+    private Cord start;
+    private Cord end;
     public void readInput(Path path) {
         try (BufferedReader br = Files.newBufferedReader(path)) {
             boolean opLines = true;
             String line;
             while ((line = br.readLine()) != null) {
+                List<Character> row = new ArrayList<>();
+                for (Character c: line.toCharArray()) {
+                    row.add(c);
+                }
+                board.add(row);
             }
+            findStartEnd();
         } catch (IOException ioe) {
             throw new IllegalStateException("Cannot read file: " + path, ioe);
+        }
+    }
+
+    private void findStartEnd() {
+        for (int x = 0; x < board.getFirst().size(); x++) {
+            if (board.getFirst().get(x) == '.') {
+                start = new Cord(x, 0);
+            }
+
+            if (board.getLast().get(x) == '.') {
+                end = new Cord(x, board.size()-1);
+            }
         }
     }
     public long partOne() {
